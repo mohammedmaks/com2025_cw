1ddb0c1260afe949e910223666d68b415555f0c9
Liman Makama <lm01081@surrey.ac.uk>
2019-12-10 01:53:51 +0000

    Added testing to the home_controller.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+    assert_select 'title', 'Football League Home'
+    assert_select 'h1', 'The idea of this page is to be the home page of the application.'
+
   end
 
 end
