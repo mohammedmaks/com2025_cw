7b15fb436710f38c4f3ca5d92f4ba45e38ef5d93
Liman Makama <lm01081@surrey.ac.uk>
2019-12-10 01:34:17 +0000

    added a few styles to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #resources: articles
+  #get 'home/contact/', to: 'home#contact'
   root'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
