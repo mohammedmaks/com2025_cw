5fd49a5a7018562e87dea090f9f4820721837ee8
Liman Makama <lm01081@surrey.ac.uk>
2019-12-10 03:54:29 +0000

    Added a contact page with relevant tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -8,5 +8,20 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h1', 'The idea of this page is to be the home page of the application.'
 
   end
+#A test for the contact controller.
+#tests whether the contact page is retrieved successfully
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+
+    assert_template layout: 'application'
+
+    assert_select 'title', 'Contact Us'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Get in touch with us by completing the following form.'
+
+
+  end
 
 end
