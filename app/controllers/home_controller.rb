3c2bdf85016d810dc63009f0f90fe76dd9a79466
Liman Makama <lm01081@surrey.ac.uk>
2019-12-12 00:36:56 +0000

    added a bootstrap nav bar with links to various pages, as well as relevant styling with scss

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,20 @@ class HomeController < ApplicationController
 
   def contact
   end
+
+  def request_contact
+    name=params[:name]
+    email=params[:email]
+    telephone=params[:telephone]
+    messgae=params[:message]
+
+    if email.blank?
+      flash[:alert] = I18n.t('home.request_contact.no_email')
+    else
+      #send email
+      flash[:notice] = I18n.t('home.request_contact.email_sent')
+  end
+
+  redirect_to_root_path
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #resources: articles
   get 'contact', to: 'home#contact'
+  post 'request_contact', to: 'home#request_contact'
 
   root'home#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
