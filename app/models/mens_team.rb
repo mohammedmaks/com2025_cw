acc24d6bc78bc4d461ab63848070449df5060d8d
Liman Makama <lm01081@surrey.ac.uk>
2019-12-13 16:34:41 +0000

    added validations to mensTeam model class

--- app/models/mens_team.rb
+++ app/models/mens_team.rb
@@ -1,2 +1,8 @@
 class MensTeam < ApplicationRecord
+  #validations to reflect field and table requirements/restrictions
+  validates :level, presence: true
+  validates :level, uniqueness: true
+  validates :division, presence: true
+  
+
 end
